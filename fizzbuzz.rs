f36ce9ff2a64e1062e489231e30db90d7d361638
Daria Jung <djj2115cu@gmail.com>
2014-07-07 22:46:29 -0400

    fizzbuzz in rust

---
+++ fizzbuzz.rs
@@ -0,0 +1,39 @@
+
+fn main() {
+    for number in range(1i, 101) {
+
+        /* allow mutability */
+        let output = 
+
+        if div_fifeteen(number) {
+            "FizzBuzz".to_str()
+        }
+
+        else if div_five(number) {
+            "Fizz".to_str()
+        }
+
+        else if div_three(number) {
+            "Buzz".to_str()
+        }
+
+        else {
+            number.to_str()
+        };
+
+        println!("{:s}", output);
+
+    }
+}
+
+fn div_fifeteen(number: int) -> bool {
+    number % 15 == 0
+}
+
+fn div_five(number: int) -> bool {
+    number % 5 == 0
+}
+
+fn div_three(number: int) -> bool {
+    number % 3 == 0
+}
